5cc36ff3a59f8034f20d5b404758912d0ddd0477
marcfreir <marcfreir@outlook.com>
2023-04-13 20:25:23 -0300

    Add functionality to draw on canvas

--- src/geoAnnotator/geo_annotator.py
+++ src/geoAnnotator/geo_annotator.py
@@ -21,7 +21,19 @@ window.config(menu=main_menu)
 print(os.path.expanduser('~\Documents'))
 
 
+################### TO DRAW POLYGON ON IMAGE - BEGIN ##########
 
+# ***** global variable declaration *****
+done = False
+points = []
+current = (0,0)
+prev_current = (0,0)
+img_number = 0
+
+################### TO DRAW POLYGON ON IMAGE - END ##########
+
+
+####MAYBE DO NOT SHOW THE TKINTER TOP LEVEL, INSTEAD SHOW DIRECTLY THE CV2 "NAMEDWINDOW"
 def new_project_window():
     top_window = Toplevel(window)
     top_window.geometry("640x640")
@@ -42,7 +54,6 @@ def new_project_window():
         top_window.destroy()
         top_window.update()
 
-    ##############test############
 
     # Open image
     def open_image():
@@ -51,6 +62,8 @@ def new_project_window():
         # Open a file chooser dialog and allow the user to select an input
         # Image
         img_path = filedialog.askopenfilename()
+        
+        global image_to_open
         # ensure a file path was selected
         if len(img_path) > 0:
             # load the image from disk
@@ -60,6 +73,7 @@ def new_project_window():
             # ...and then to ImageTk format
             image_to_open = ImageTk.PhotoImage(image_to_open)
 
+
             # if the panels are None, initialize them
             if panel_image is None:
                 # The panel image will store our original image
@@ -67,6 +81,74 @@ def new_project_window():
                 panel_image.image_to_open = image_to_open
                 panel_image.pack(side="left", padx=5, pady=5)
                 #pass
+
+
+                new_image = cv2.imread(img_path)
+                image_clone = new_image.copy()
+                image_temp = new_image.copy()
+
+                def on_mouse(event, x, y, buttons, user_param):
+                    global done, points, current,image_temp
+                    # Mouse callback that gets called for every mouse event (i.e. moving, clicking, etc.)
+                    if done: # Nothing more to do
+                        return
+                    if event == cv2.EVENT_MOUSEMOVE:
+                        # We want to be able to draw the line-in-progress, so update current mouse position
+                        current = (x, y)
+                    elif event == cv2.EVENT_LBUTTONDOWN:
+                        # Left click means adding a point at current position to the list of points
+                        print("Adding point #%d with position(%d,%d)" % (len(points), x, y))
+                        cv2.circle(new_image,(x,y),5,(0,200,0),-1)
+                        points.append([x, y])
+                        image_temp = new_image.copy()
+                    elif event == cv2.EVENT_RBUTTONDOWN:
+                        # Right click means we're done
+                        print("Completing polygon with %d points." % len(points))
+                        done = True
+
+                cv2.namedWindow("image")
+                cv2.setMouseCallback("image", on_mouse)
+
+                
+                global done
+                while(not done):
+                    # This is our drawing loop, we just continuously draw new images
+                    # and show them in the named window
+                    if (len(points) > 1):
+                        if(current != prev_current):
+                            new_image = image_temp.copy()
+                        # Draw all the current polygon segments
+                        cv2.polylines(new_image, [np.array(points)], False, (255,0,0), 1)
+                        # And  also show what the current segment would look like
+                        cv2.line(new_image, (points[-1][0],points[-1][1]), current, (255,0,255))
+
+                    # --->>> Create a new thread for the instanciated window <<<<-----
+                    cv2.startWindowThread()
+                    # Update the window
+                    cv2.imshow("image", new_image)
+                    # And wait 50ms before next iteration (this will pump window messages meanwhile)
+
+                    if cv2.waitKey(50) == ord('d'): # press d(done)
+                        done = True
+                        
+
+                # User finised entering the polygon points, so let's make the final drawing
+                new_image = image_clone.copy()
+                # of a filled polygon
+                if (len(points) > 0):
+                    cv2.fillPoly(new_image, np.array([points]), (255,0,0))
+                # And show it
+                cv2.imshow("image", new_image)
+
+                # Save the image drawn on canvas
+                #save_file_to_path
+                #img = cv2.imwrite(save_file_to_path)
+
+                # Waiting for the user to press any key
+                cv2.waitKey(0)
+                #cv2.destroyWindow("image")
+                cv2.destroyAllWindows()
+
 
             else:
                 # update the panel
@@ -76,9 +158,6 @@ def new_project_window():
 # initialize the image panels
 #panel_image = None
 
-    
-
-    ##############test############
 
     # Add new project file menu
     new_project_menu = Menu(new_project_window_menu, tearoff=False)
@@ -87,10 +166,6 @@ def new_project_window():
     new_project_menu.add_command(label="Open image in project", command=open_image)
     new_project_menu.add_command(label="Exit", command=exit_new_project)
     
-
-# TEMP: Just a test button
-#test_button = Button(window, text="Open window", command=new_project_window)
-#test_button.pack(pady=50, padx=50)
 
 panel_image = None
 
